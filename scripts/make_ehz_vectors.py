27ff709226a9ff252387d2b74219c3d50bffa74a
Jon Connolly <jontconnolly@gmail.com>
2018-05-24 11:33:41 -0700

    modify process to create vectors and plot into two unique steps

---
+++ scripts/make_ehz_vectors.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+from magD.magD import MagD
+if len(sys.argv)<2:
+    print("provide config path as first arg and map output type for second arg")
+    print("Example: python scripts/ehz_profile.py config/ehz_profile/ehz_and_bb.ini")
+    exit(1)
+config_path =sys.argv[1]
+magD=MagD(config_path)
+out=magD.create_vectors()
+print(out)
--- scripts/plot_ehz_profile.py
+++ scripts/plot_ehz_profile.py
@@ -1,42 +1,20 @@
 import sys
 import os
-from pathlib import Path
 
 from magD.plotMagD import PlotMagD
 from magD.magD import MagD
-if len(sys.argv)<2:
-    print("provide config path as first arg")
-    print("Example: python scripts/ehz_profile.py config/ehz_profile/ehz_and_bb.ini")
+if len(sys.argv)<3:
+    print("provide config path as first arg and map output type for second arg")
+    print("Example: python scripts/ehz_profile.py config/ehz_profile/ehz_and_bb.ini detection")
+    print("available map types are detection and gap and distance")
     exit(1)
 config_path =sys.argv[1]
-magD=MagD(config_path)
-other_mag_grid_path=None
-if magD.diff_with:
-    other_mag_grid_path=magD.get_pickle_grid_path('mag_grid',
-                magD.diff_with, magD.grid_resolution)
-    other_mag_grid_file= Path(other_mag_grid_path)
-    if not other_mag_grid_file.is_file():
-        print("Other diff file not found!")
-        print("Expected: {}".format(other_mag_grid_path))
-        exit(1)
+type=sys.argv[2]
+magD=MagD(config_path,type)
+pm=PlotMagD(magD,type)
 
-magD.get_noise()
-#only go through find detections if pickle file doesn't exist
-this_mag_grid_path=magD.get_pickle_grid_path('mag_grid',
-            magD.name, magD.grid_resolution)
-this_mag_grid_file= Path(this_mag_grid_path)
-if this_mag_grid_file.is_file():
-    mag_grid= magD.get_pickle(this_mag_grid_path)
-else:
-    print("no file here...")
-    magD.find_detections()
-    mag_grid=magD.get_mag_grid()
-    magD.pickle_mag_grid()
-    sum=magD.print_summary()
-    print(sum)
+magD.read_vectors()
 
-
-pm=PlotMagD(magD)
 fig=pm.plot().figure(figsize=(10,12))
 pm.plot().rc("font", size=14)
 
@@ -46,20 +24,15 @@ map.drawcoastlines(zorder=1)
 map.drawstates(zorder=2)
 map.drawcountries(zorder=2)
 
-# diff the two matrices
-if other_mag_grid_path:
-    other_path=magD.get_pickle_grid_path('mag_grid',
-        magD.diff_with, magD.grid_resolution)
-    other_mag_grid=magD.get_pickle(other_path)
-    mag_grid = [a - b  for a, b in zip(mag_grid, other_mag_grid)]
 
-# levels=pm.create_contour_levels(mag_grid, 2)
+
+# levels=pm.create_contour_levels(detect_vector, 2)
 X,Y=pm.project_x_y(map)
-Z= pm.mag_matrix(mag_grid)
-min=float(magD.plot_mag_min)
-max=float(magD.plot_mag_max)
-
-pm.plot().pcolormesh(X,Y,Z,zorder=0, vmin=min, vmax=max)
+Z= pm.make_matrix(len(magD.lat_list()), len(magD.lon_list()))
+if pm.type=="detection":
+    pm.plot().pcolormesh(X,Y,Z,zorder=0, vmin=pm.mag_min, vmax=pm.mag_max)
+else:
+    pm.plot().pcolormesh(X,Y,Z,zorder=0)
 pm.plot().colorbar()
 # cs =map.contour(X,Y,Z,levels, colors="k")#,colors="k",zorder=3,linewidths=0.5)
 
@@ -76,11 +49,19 @@ dashes=[90,8], linewidth=0.0)
 
 # #zorder puts it at lowest plot level
 # map.fillcontinents(color='0.95',zorder=1)
+header=""
+if type=="detection":
+    header="Minimum Magnitude Detection"
+elif type=="gap":
+    header="Largest Azimuthal Gap"
+else:
+    header="Distance to closest station"
+
 pm.plot().title(
-    "{}\n {} station detection {} deg. grid".format(
-    magD.title, magD.num_detections, magD.grid_resolution))
+    "{}\n{}\n {} station detection {} deg. grid".format(
+    header,pm.title, magD.num_detections, magD.grid_resolution))
 # #should we add no solution symbol to legend?
-no_solution=False
+# no_solution=False
 #iterate through sets and assign color
 
 # for key in magD.scnl_collections():
@@ -102,11 +83,11 @@ no_solution=False
 #   #plot no solutions but don't create a legend entry for each
 #   pm.plot().scatter(Sxn, Syn, s=30, marker='o', facecolors='none', edgecolors=color,zorder=11)
 
-#create legend for no solutions
-if no_solution:
-  pm.plot().scatter([-1],[-1], s=30, marker='o',facecolors='none',edgecolor='k',label="No solution")
+# #create legend for no solutions
+# if no_solution:
+#   pm.plot().scatter([-1],[-1], s=30, marker='o',facecolors='none',edgecolor='k',label="No solution")
 #bbox coords= x,y,width,height
-bbox=(0.0,-0.2)
+# bbox=(0.0,-0.2)
 # pm.plot().legend(bbox_to_anchor=bbox, loc=3, borderaxespad=0.,scatterpoints=1)
 
 plot_path="./plots/{}".format(magD.name)
